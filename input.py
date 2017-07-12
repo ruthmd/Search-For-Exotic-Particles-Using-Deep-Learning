bfb56584a32bd36f8a53f2ce0d4d9914963c8082
happystick <sai.sasank.yadati@gmail.com>
2017-07-12 15:46:34 +0530

    Created input.py

---
+++ input.py
@@ -0,0 +1,4 @@
+# SCRIPT TO BUILD AN INPUT PIPELINE
+
+# write a function that takes file path and returns an iterator
+# checkout 'Reading from Files' at https://www.tensorflow.org/programmers_guide/reading_data for more info on this
