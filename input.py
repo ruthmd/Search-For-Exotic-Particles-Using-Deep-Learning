2472446a92f87a60421111610477f84c7aaa7528
Ruthwik <ruthwikganesh@gmail.com>
2017-07-23 09:50:21 +0530

    Read 10 examples from dataset

--- input.py
+++ input.py
@@ -1,4 +1,38 @@
 # SCRIPT TO BUILD AN INPUT PIPELINE
+import tensorflow as tf
+import os
 
-# write a function that takes file path and returns an iterator
-# checkout 'Reading from Files' at https://www.tensorflow.org/programmers_guide/reading_data for more info on this
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+data_path = "data/"
+
+
+def get_data_from_csv():
+
+    filename_queue = tf.train.string_input_producer([data_path + "higgs.csv"])
+    reader = tf.TextLineReader()
+    _, row = reader.read(filename_queue)
+
+    defaults = [[0.0] for x in range(0, 29)]
+    
+    a = [x for x in tf.decode_csv(row, record_defaults=defaults)]
+
+    features = tf.unstack([x for x in a[1:]])
+
+    init = tf.global_variables_initializer()
+    with tf.Session() as sess:
+        sess.run(init)
+
+        coord = tf.train.Coordinator()
+        threads = tf.train.start_queue_runners(coord=coord)
+
+        # grabbing 10 examples from the CSV file.
+        for iteration in range(0, 10):
+            example, label = sess.run([features, a[0]])
+
+            print(example, label)
+        coord.request_stop()
+        coord.join(threads)
+
+
+get_data_from_csv()
