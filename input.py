6f16bd63afd319bd54ec3e9653253871cca2f0ec
happystick <sai.sasank.yadati@gmail.com>
2017-07-23 21:21:58 +0530

    Modified input.py

--- input.py
+++ input.py
@@ -1,38 +1,69 @@
-# SCRIPT TO BUILD AN INPUT PIPELINE
-import tensorflow as tf
 import os
+os.environ['TF_CPP_MIN_LOG_LEVEL']='2'
 
-os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+import tensorflow as tf
 
-data_path = "data/"
+BATCH_SIZE = 32
+N_FEATURES = 28
+
+def batch_generator():
+    """ filenames is the list of files you want to read from.
+    In this case, it contains only heart.csv
+    """
+    filename_queue = tf.train.string_input_producer(['higgs.csv'])
+    reader = tf.TextLineReader() # skip the first line in the file
+    _, value = reader.read(filename_queue)
+
+    # record_defaults are the default values in case some of our columns are empty
+    # This is also to tell tensorflow the format of our data (the type of the decode result)
+    # for this dataset, out of 9 feature columns,
+    # 8 of them are floats (some are integers, but to make our features homogenous,
+    # we consider them floats), and 1 is string (at position 5)
+    # the last column corresponds to the lable is an integer
+
+    record_defaults = [[0.0] for _ in range(N_FEATURES+1)]
+
+    # read in the 10 rows of data
+    content = tf.decode_csv(value, record_defaults=record_defaults)
 
 
-def get_data_from_csv():
+    # pack all 28 features into a tensor
+    features = tf.stack(content[:N_FEATURES])
 
-    filename_queue = tf.train.string_input_producer([data_path + "higgs.csv"])
-    reader = tf.TextLineReader()
-    _, row = reader.read(filename_queue)
+    # assign the first column to label
+    label = content[0]
 
-    defaults = [[0.0] for x in range(0, 29)]
-    
-    a = [x for x in tf.decode_csv(row, record_defaults=defaults)]
+    # minimum number elements in the queue after a dequeue, used to ensure
+    # that the samples are sufficiently mixed
+    # I think 10 times the BATCH_SIZE is sufficient
+    min_after_dequeue = 10 * BATCH_SIZE
 
-    features = tf.unstack([x for x in a[1:]])
+    # the maximum number of elements in the queue
+    capacity = 20 * BATCH_SIZE
 
-    init = tf.global_variables_initializer()
+    # shuffle the data to generate BATCH_SIZE sample pairs
+    data_batch, label_batch = tf.train.shuffle_batch([features, label], batch_size=BATCH_SIZE,
+                                        capacity=capacity, min_after_dequeue=min_after_dequeue)
+
+    return data_batch, label_batch
+
+def generate_batches(data_batch, label_batch):
     with tf.Session() as sess:
-        sess.run(init)
-
         coord = tf.train.Coordinator()
         threads = tf.train.start_queue_runners(coord=coord)
-
-        # grabbing 10 examples from the CSV file.
-        for iteration in range(0, 10):
-            example, label = sess.run([features, a[0]])
-
-            print(example, label)
+        for _ in range(2): # generate 10 batches
+            features, labels = sess.run([data_batch, label_batch])
+            print(features, labels)
         coord.request_stop()
         coord.join(threads)
 
+def main():
+    data_batch, label_batch = batch_generator()
+    generate_batches(data_batch, label_batch)
+    print("New batch!")
+    data_batch, label_batch = batch_generator()
+    generate_batches(data_batch, label_batch)
+    print("New batch!")
 
-get_data_from_csv()
+if __name__ == '__main__':
+    main()
