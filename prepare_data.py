ab37279d65b84a5e1ecac8bf7aa9798547999e5a
happystick <sai.sasank.yadati@gmail.com>
2017-08-29 00:15:53 +0530

    Mini Myni Changes

---
+++ prepare_data.py
@@ -0,0 +1,23 @@
+""" helper code to split the datasets """
+
+import pandas as pd
+import numpy as np
+
+np.random.seed(42)
+
+DATA_PATH = "data/HIGGS.csv"
+
+chunksize = 10000
+
+chunk_train = pd.read_csv(DATA_PATH, chunksize=chunksize, skiprows=0, nrows=660000)
+chunk_valid = pd.read_csv(DATA_PATH, chunksize=chunksize, skiprows=660000, nrows=220000)
+chunk_test = pd.read_csv(DATA_PATH, chunksize=chunksize, skiprows=880000, nrows=220000)
+
+for chunk in chunk_train:
+    chunk.to_csv('data/train.csv', sep=',', mode='a')
+
+for chunk in chunk_valid:
+    chunk.to_csv('data/valid.csv', sep=',', mode='a')
+
+for chunk in chunk_test:
+    chunk.to_csv('data/test.csv', sep=',', mode='a')
